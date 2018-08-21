3b96ba4ace425c7458f2810be919be5e4338a63c
Imam <imam.kurniansyah@docotel.com>
2018-08-21 12:18:07 +0700

    Add new Presenter and Adapter

--- app/src/main/java/koin/example/snapnoob/insertkoin/di/AppModule.kt
+++ app/src/main/java/koin/example/snapnoob/insertkoin/di/AppModule.kt
@@ -4,9 +4,17 @@ import io.reactivex.disposables.CompositeDisposable
 import koin.example.snapnoob.insertkoin.data.AppDataManager
 import koin.example.snapnoob.insertkoin.data.network.AppApiHelper
 import koin.example.snapnoob.insertkoin.data.preferences.AppPreferenceHelper
+import koin.example.snapnoob.insertkoin.ui.detailMovie.presenter.DetailPresenter
+import koin.example.snapnoob.insertkoin.ui.detailMovie.presenter.DetailPresenterContract
+import koin.example.snapnoob.insertkoin.ui.detailMovie.view.DetailActivityContract
 import koin.example.snapnoob.insertkoin.ui.login.presenter.LoginPresenter
 import koin.example.snapnoob.insertkoin.ui.login.presenter.LoginPresenterContract
 import koin.example.snapnoob.insertkoin.ui.login.view.LoginActivityContract
+import koin.example.snapnoob.insertkoin.ui.main.NowPlayingAdapter
+import koin.example.snapnoob.insertkoin.ui.main.UpcomingAdapter
+import koin.example.snapnoob.insertkoin.ui.main.presenter.MainPresenter
+import koin.example.snapnoob.insertkoin.ui.main.presenter.MainPresenterContract
+import koin.example.snapnoob.insertkoin.ui.main.view.MainActivityContract
 import koin.example.snapnoob.insertkoin.util.AppConstants
 import koin.example.snapnoob.insertkoin.util.SchedulerProvider
 import org.koin.dsl.module.Module
@@ -20,8 +28,16 @@ val appModule: Module = applicationContext {
     factory { CompositeDisposable() }
 }
 
-val loginModule: Module = applicationContext {
-    factory { LoginPresenter<LoginActivityContract>(get(), get(), get()) as LoginPresenterContract<LoginActivityContract> }
+val presenterModule: Module = applicationContext {
+    factory { LoginPresenter<LoginActivityContract>(get(), get(), get(), get()) as LoginPresenterContract<LoginActivityContract> }
+    factory { MainPresenter<MainActivityContract>(get(), get(), get()) as MainPresenterContract<MainActivityContract>}
+    factory { DetailPresenter<DetailActivityContract>(get(), get(), get()) as DetailPresenterContract<DetailActivityContract> }
 }
 
-val mainModule = listOf(appModule, loginModule)
+val adapterModule: Module = applicationContext {
+    factory { NowPlayingAdapter(ArrayList()) }
+    factory { UpcomingAdapter(ArrayList()) }
+}
+
+val allModule = listOf(appModule, presenterModule, adapterModule)
+
